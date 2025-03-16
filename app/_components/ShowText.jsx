227b44677a58ec983feed75969062ce594d6a7a9
mohammad <ghaffari8343@gmail.com>
2025-03-16 23:24:21 +0330

    Updated feature

--- app/_components/ShowText.jsx
+++ app/_components/ShowText.jsx
@@ -6,16 +6,19 @@ const sentence = "حامی تیم، گروه تخصصی رزرو هتل";
 export default function AnimatedText() {
   return (
     <div className="flex justify-center items-center">
-      <motion.h1 className="text-2xl md:text-4xl font-bold text-gray-300  word-spacing-[0.15em]" dir="rtl">
-        {Array.from(sentence).map((char, index) => (
+      <motion.h1
+        className="text-2xl md:text-4xl font-bold text-gray-300 leading-relaxed"
+        dir="rtl"
+      >
+        {sentence.split(" ").map((word, index) => (
           <motion.span
             key={index}
             initial={{ opacity: 0, x: -10 }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.05, delay: index * 0.1 }}
-            
+            transition={{ duration: 0.2, delay: index * 0.2 }}
+            className="inline-block mx-1"
           >
-            {char}
+            {word}
           </motion.span>
         ))}
       </motion.h1>
